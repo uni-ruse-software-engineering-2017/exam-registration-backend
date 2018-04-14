692773f4ff83b6d053c2a3fda215e993d0494790
xTrEeMx <33663467+xTrEeMx@users.noreply.github.com>
2018-04-14 14:12:00 +0300

    Fix rest mapping for UserResource

--- src/main/java/uniruse/mse/examregistration/user/UserResource.java
+++ src/main/java/uniruse/mse/examregistration/user/UserResource.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/users")
+@RestController
+@RequestMapping("/users")
 public class UserResource {
 
 	@Autowired
